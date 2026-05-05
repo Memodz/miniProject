f6ca55ddf22e4facc84e4fb6a7481d29d349faf8
Phoungpaga Loungprom <phoungpagaloungprom@Mmooddzz-2.local>
2026-05-05 22:29:00 +0700

    add comment V2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Hello from my DevOps app!", "status": "running"})
+    return jsonify({"message": "Hello from my DevOps app V2!", "status": "running"})
 
 @app.route("/health")
 def health():
